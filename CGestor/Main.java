04e9e62048ff09110a8f9f18c43e2dedac846682
imanolpg <imanolgutierrez@yahoo.es>
2019-10-09 10:18:53 +0200

    Empezada la gestion de la base de datos

--- CGestor/Main.java
+++ CGestor/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 	
@@ -12,7 +13,8 @@ public class Main {
 	
 	static Canvas canvas;
 	static JPanel panelGeneral;
-	static JPanel panelEmail; 
+	static JPanel panelEmail;
+	static ArrayList<String[]> actualizaciones;
 
 	public static void main(String[] args) {
 		panelGeneral = new panelGeneral();
@@ -65,15 +67,52 @@ public class Main {
 		canvas.setVisible(true);
 	}
 	
+	public static void updateDatabase() {
+		 String sql = "DELETE FROM warehouses WHERE id = ?";
+		 
+		for (String[] orden : actualizaciones) {
+		    try (Connection conn = this.connect();
+		    	PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+		    	// set the corresponding param
+		    	pstmt.setInt(1, id);
+		    	// execute the delete statement
+		    	pstmt.executeUpdate();
+		 
+		        } catch (SQLException e) {
+		            System.out.println(e.getMessage());
+		        }
+		}
+	
+	
 	/** Ordena los datos para mostrarlos en la tabla
 	 * @return Object[][] de los datos
 	 */
 	public static Object[][] getDatosTabla(){
 		Connection conexion;
+		Statement stmt;
+		ResultSet resultado;
+		ArrayList<String[]> participantes;
+		participantes = new ArrayList<String[]>();
 		try {
 			Class.forName("org.sqlite.JDBC");
 			conexion = DriverManager.getConnection("jdbc:sqlite:test.db");
-			
+			stmt = conexion.createStatement();
+			resultado = stmt.executeQuery("SELECT * FROM familias");
+	        while (resultado.next()) {
+	        	String[] familia = {
+	        			resultado.getString("id"),
+	        			resultado.getString("nombre"),
+	        			resultado.getString("participantes"),
+	        			resultado.getString("haPagado"),
+	        			resultado.getString("tallasDisfraces"),
+	        			resultado.getString("telefono"),
+	        			resultado.getString("email")
+	        	};
+	        	participantes.add(familia);
+	        }
+			//stmt.executeUpdate(sql);
+			stmt.close();
 		}catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 		}
@@ -198,7 +237,7 @@ public class Main {
 				
 		};
 
-		return(datos);
+		return (Object[][]) (participantes.toArray());
 	}
 
 	/** Devuelve las columnas que va a llevar la tabla
